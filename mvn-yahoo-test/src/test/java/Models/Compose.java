2380904983062ff21177c4bc127c9741219c5262
Lex <Lex@Lex-Mobile>
2014-07-18 10:03:20 +0700

    Initial project

---
+++ mvn-yahoo-test/src/test/java/Models/Compose.java
@@ -0,0 +1,50 @@
+package Models;
+
+public class Compose {
+	private String to;
+	private String cc;
+	private String bcc;
+	private String subject;
+	private String body;
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getCc() {
+		return cc;
+	}
+
+	public void setCc(String cc) {
+		this.cc = cc;
+	}
+
+	public String getBcc() {
+		return bcc;
+	}
+
+	public void setBcc(String bcc) {
+		this.bcc = bcc;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+}
